8cbed623cb08c1c06590c94f7771cc1dd8e59de8
Vladimir Sokolov <sokovlad1@mail.ru>
2016-02-02 14:34:43 +0600

    Manipulations with path variables

--- src/ru/sokolov/springmvc/controllers/HelloController.java
+++ src/ru/sokolov/springmvc/controllers/HelloController.java
@@ -1,17 +1,23 @@
 package ru.sokolov.springmvc.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
+	@RequestMapping("/welcome/{countryName}/{userName}")
+	public ModelAndView helloWorld(@PathVariable Map<String, String> pathVars) {
+		
+		String name = pathVars.get("userName");
+		String country = pathVars.get("countryName");
 		
 		ModelAndView modelandview = new ModelAndView("HelloPage");
-		modelandview.addObject("welcomeMessage", "Hi User, welcome to the first Spring MVC Application");
+		modelandview.addObject("welcomeMessage", "Hi, "+name+" from "+country+". Welcome to the first Spring MVC Application!");
 		
 		return modelandview;
 	}
